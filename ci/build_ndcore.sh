a1abada918ec4331b6a786f89432a789be32d41f
Juan Jose Garcia Cantero <juanjose.garcia@openbraininstitute.org>
2025-01-22 15:41:31 +0100

    Adapts integration test

--- ci/build_ndcore.sh
+++ ci/build_ndcore.sh
@@ -11,11 +11,11 @@ if [ -d _lib ]; then
 fi
 
 # Get the common synapses
-COMMON_DIR=_common
+COMMON_DIR=neurodamus-models/common
 if [ -d "$COMMON_DIR" ]; then
     ( cd "$COMMON_DIR" && git pull --quiet )
 else
-    git clone git@bbpgitlab.epfl.ch:hpc/sim/models/common.git $COMMON_DIR  --depth=1
+    git clone https://github.com/openbraininstitute/neurodamus-models.git neurodamus-models  --depth=1
 fi
 
 MOD_BUILD_DIR="mods.tmp"
@@ -23,12 +23,13 @@ mkdir -p $MOD_BUILD_DIR
 cp -f $CORE_DIR/mod/*.mod $MOD_BUILD_DIR
 cp -f $COMMON_DIR/mod/*.mod $MOD_BUILD_DIR
 nrnivmodl -incflags "-DDISABLE_REPORTINGLIB -DDISABLE_HDF5 -DDISABLE_MPI" $MOD_BUILD_DIR
-if [ ! -f x86_64/special ]; then
+ARCH=$(uname -m)
+if [ ! -f $ARCH/special ]; then
     echo "Error running nrnivmodl"
     exit 1
 fi
 mkdir -p _lib
-cp -f x86_64/libnrnmech* _lib/
+cp -f $ARCH/libnrnmech* _lib/
 cp -f $CORE_DIR/hoc/*.hoc _lib/
 cp -f $COMMON_DIR/hoc/*.hoc _lib/
 
