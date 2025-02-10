c93fa0c24b4f9e0c7133b48a55e96214cfb2a973
Weina Ji <weina.ji@openbraininstitute.org>
2025-02-10 16:05:25 +0100

    fixture ringtest config

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,28 @@
 import pytest
+from pathlib import Path
 # !! NOTE: Please don't import NEURON/Neurodamus at module level
 # pytest weird discovery system will trigger NEURON init and open a can of worms
 
 # Make all tests run forked
 pytestmark = pytest.mark.forked
+
+RINGTEST_DIR = Path(__file__).parent.parent.absolute() / "simulations" / "ringtest"
+
+
+@pytest.fixture
+def ringtest_baseconfig():
+    return dict(
+        manifest={"$CIRCUIT_DIR": str(RINGTEST_DIR)},
+        network="$CIRCUIT_DIR/circuit_config.json",
+        node_sets_file="$CIRCUIT_DIR/nodesets.json",
+        run={
+            "random_seed": 1122,
+            "dt": 0.1,
+            "tstop": 50,
+        },
+        node_set="Mosaic",
+        conditions={
+            "celsius": 35,
+            "v_init": -65
+        }
+    )
--- tests/integration/test_ringcells.py
+++ tests/integration/test_ringcells.py
@@ -1,23 +1,13 @@
+import json
 import os
+import pytest
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 
 SIM_DIR = Path(__file__).parent.parent.absolute() / "simulations" / "ringtest"
-REF_DIR = Path(__file__).parent.parent.absolute() / "simulations" / "ringtest" / "reference"
+REF_DIR = SIM_DIR / "reference"
 CONFIG_FILE = str(SIM_DIR / "simulation_config.json")
-
-
-def test_dumpcellstates():
-    from neurodamus import Neurodamus
-    from neurodamus.core import NeurodamusCore as Nd
-    n = Neurodamus(CONFIG_FILE, disable_reports=True)
-    from neurodamus.utils.dump_cellstate import dump_cellstate
-    test_gids = [1, 1001]
-    for gid in test_gids:
-        dump_cellstate(n._pc, Nd.cvode, gid)
-        outputfile = "cellstate_" + str(gid) + ".json"
-        reference = REF_DIR / outputfile
-        compare_json_files(outputfile, str(reference))
 
 
 def compare_json_files(res_file, ref_file):
@@ -29,3 +19,32 @@ def compare_json_files(res_file, ref_file):
     with open(ref_file) as f_ref:
         reference = json.load(f_ref)
     assert result  == reference
+
+
+@pytest.fixture
+def simconfig_nodeset(ringtest_baseconfig, extra_config):
+    ringtest_baseconfig.update(extra_config)
+
+    with NamedTemporaryFile("w", suffix='.json', delete=False) as config_file:
+        json.dump(ringtest_baseconfig, config_file)
+
+    yield config_file
+    os.unlink(config_file.name)
+
+
+@pytest.mark.parametrize("extra_config", [{"node_set": "Mosaic"}])
+def test_dump_allcells(simconfig_nodeset):
+    from neurodamus import Neurodamus
+    from neurodamus.core import NeurodamusCore as Nd
+    n = Neurodamus(simconfig_nodeset.name, disable_reports=True)
+    from neurodamus.utils.dump_cellstate import dump_cellstate
+    test_gids = [1, 1001]
+    for gid in test_gids:
+        dump_cellstate(n._pc, Nd.cvode, gid)
+        outputfile = "cellstate_" + str(gid) + ".json"
+        reference = REF_DIR / outputfile
+        compare_json_files(outputfile, str(reference))
+
+
+# @pytest.mark.parametrize("extra_config", [{"node_set": "RingB"}])
+# def test_dump_RingB():
