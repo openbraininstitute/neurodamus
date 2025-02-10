2cab5bb7619c3578205899363af9c933c8641eec
MikeG <mgevaert@openbraininstitute.org>
2025-02-10 11:08:42 +0100

    fix lints for neurodamus/__init__.py (#48)

--- neurodamus/__init__.py
+++ neurodamus/__init__.py
@@ -1,7 +1,4 @@
-# flake8: noqa
-"""
-neurodamus
-----------
+"""neurodamus
 
 The neurodamus package implements the instantiation of simulations in Neuron
 based on a configuration file, a.k.a. simulation_config.json
@@ -9,19 +6,17 @@ It is deeply based on the HOC implementation, therefore providing python modules
 `node`, `cell_distributor`, etc; and still depends on several low-level HOC files at runtime.
 """
 
-from __future__ import absolute_import
 import importlib.metadata
 
 try:
     __version__ = importlib.metadata.version(__name__)
-except Exception:
+except importlib.metadata.PackageNotFoundError:
     __version__ = "devel"
 
 __author__ = "Fernando Pereira <fernando.pereira@epfl.ch>"
 __copyright__ = "2018 Blue Brain Project, EPFL"
 
 # Neurodamus node for setting up a neurodamus execution
-from .node import Node, Neurodamus
+from .node import Neurodamus, Node
 
-
-__all__ = ["Node", "Neurodamus"]
+__all__ = ["Neurodamus", "Node"]
