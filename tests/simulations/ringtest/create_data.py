502b7ef3e78ed707d824f46b3a6fcab56bbbeffc
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-02-25 10:10:27 +0100

    Make it easier to parameterize building small circuits

--- tests/simulations/ringtest/create_data.py
+++ tests/simulations/ringtest/create_data.py
@@ -3,159 +3,211 @@
 # dependencies = ['h5py', 'libsonata', 'numpy']
 # ///
 # the above allows one to run `uv run create_data.py` without a virtualenv
+import itertools as it
+from dataclasses import dataclass
+from collections.abc import Iterable, Sequence
 
 import h5py
 import libsonata
 import numpy as np
 
 
-def make_nodes():
-    name = "RingA"
-    count = 3
-    with h5py.File("nodes_A.h5", "w") as h5:
+@dataclass
+class SonataAttribute:
+    name: str
+    type: type
+    prefix: bool
+
+
+NODE_TYPES = [
+    SonataAttribute("node_type_id", type=int, prefix=False),
+    SonataAttribute("model_template", type=h5py.string_dtype(), prefix=True),
+    SonataAttribute("mtype", type=h5py.string_dtype(), prefix=True),
+    SonataAttribute("x", type=np.float32, prefix=True),
+    SonataAttribute("y", type=np.float32, prefix=True),
+    SonataAttribute("z", type=np.float32, prefix=True),
+    SonataAttribute("morphology", type=h5py.string_dtype(), prefix=True),
+    ]
+NODE_TYPES = {attr.name: attr for attr in NODE_TYPES}
+
+EDGE_TYPES = [
+    SonataAttribute("edge_type_id", type=int, prefix=False),
+    SonataAttribute("conductance", type=np.float32, prefix=True),
+    SonataAttribute("decay_time", type=np.float32, prefix=True),
+    SonataAttribute("delay", type=np.float32, prefix=True),
+    SonataAttribute("depression_time", type=np.float32, prefix=True),
+    SonataAttribute("facilitation_time", type=np.float32, prefix=True),
+    SonataAttribute("u_syn", type=np.float32, prefix=True),
+
+    SonataAttribute("afferent_section_id", type=int, prefix=True),
+    SonataAttribute("afferent_section_pos", type=np.float32, prefix=True),
+    SonataAttribute("afferent_segment_id", type=int, prefix=True),
+    SonataAttribute("afferent_segment_offset", type=int, prefix=True),
+
+    SonataAttribute("n_rrp_vesicles", type=int, prefix=True),
+    SonataAttribute("syn_type_id", type=int, prefix=True),
+    ]
+EDGE_TYPES = {attr.name: attr for attr in EDGE_TYPES}
+
+
+@dataclass
+class Edges:
+    src: str
+    tgt: str
+    type: str
+    connections: list[(int, int)]
+
+
+def _expand_values(attr, value, count):
+    if isinstance(value, str):
+        ds_value = [value] * count
+    elif isinstance(value, Sequence):
+        assert len(value) == count, f"For {attr}, {len(value)} != (count) {count}"
+        ds_value = value
+    elif isinstance(value, Iterable):
+        ds_value = list(it.islice(value, count))
+    else:
+        ds_value = [value] * count
+
+    return ds_value
+
+
+def make_node(filename, name, count, wanted_attributes):
+    with h5py.File(filename, "w") as h5:
         dg = h5.create_group(f"/nodes/{name}")
-        dg.create_dataset("0/model_template", data=["hoc:B_BallStick"] * count)
-        dg.create_dataset("node_type_id", data=[-1] * count)
 
-        # neurodamus/io/cell_readers.py:140: SonataError
-        dg.create_dataset("0/mtype", data=["MIKE"] * count)
-
-        # neurodamus/io/cell_readers.py:162: SonataError
-        dg.create_dataset("0/x", data=[0.0] * count, dtype=np.float32)
-        dg.create_dataset("0/y", data=[0.0] * count, dtype=np.float32)
-        dg.create_dataset("0/z", data=[0.0] * count, dtype=np.float32)
-
-        # Note: the morphology isn't used because it's encoded in the hoc file
-        dg.create_dataset("0/morphology", data=["NOT_USED"] * count)
-
-    name = "RingB"
-    count = 2
-    with h5py.File("nodes_B.h5", "w") as h5:
-        dg = h5.create_group(f"/nodes/{name}")
-        dg.create_dataset("node_type_id", data=[-1] * count)
-        dg.create_dataset("0/model_template", data=["hoc:B_BallStick"] * count)
-
-        # neurodamus/io/cell_readers.py:140: SonataError
-        dg.create_dataset("0/mtype", data=["MIKE"] * count)
-
-        # neurodamus/io/cell_readers.py:162: SonataError
-        dg.create_dataset("0/x", data=[0.0] * count, dtype=np.float32)
-        dg.create_dataset("0/y", data=[0.0] * count, dtype=np.float32)
-        dg.create_dataset("0/z", data=[0.0] * count, dtype=np.float32)
-
-        # Note: the morphology isn't used because it's encoded in the hoc file
-        dg.create_dataset("0/morphology", data=["NOT_USED"] * count)
+        for attr, value in wanted_attributes.items():
+            typ = NODE_TYPES[attr]
+            ds_name = ("0/" if typ.prefix else "") + typ.name
+            ds_value = _expand_values(attr, value, count)
+            dg.create_dataset(name=ds_name, data=ds_value, dtype=typ.type)
 
 
-def make_edges_AB():
-    name = "RingA__RingB__chemical"
-    count = 1
-    path = "edges_AB.h5"
-    with h5py.File(path, "w") as h5:
+def make_edges(filename, edges, wanted_attributes):
+    name = f"{edges.src}__{edges.tgt}__{edges.type}"
+    src_ids, tgt_ids = zip(*edges.connections)
+    count = len(src_ids)
+    with h5py.File(filename, "w") as h5:
         dg = h5.create_group(f"/edges/{name}")
-        dg.create_dataset("edge_type_id", data=[-1] * count)
 
-        dg.create_dataset("0/conductance", data=[1.] * count, dtype=np.float32)
-        dg.create_dataset("0/decay_time", data=[2.] * count, dtype=np.float32)
-        dg.create_dataset("0/delay", data=[3.] * count, dtype=np.float32)
-        dg.create_dataset("0/depression_time", data=[4.] * count, dtype=np.float32)
-        dg.create_dataset("0/facilitation_time", data=[5.] * count, dtype=np.float32)
-        dg.create_dataset("0/u_syn", data=[6.] * count, dtype=np.float32)
+        for attr, value in wanted_attributes.items():
+            typ = EDGE_TYPES[attr]
+            ds_name = ("0/" if typ.prefix else "") + typ.name
+            ds_value = _expand_values(attr, value, count)
+            dg.create_dataset(name=ds_name, data=ds_value, dtype=typ.type)
 
-        dg.create_dataset("0/afferent_section_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_section_pos", data=[0.75] * count, dtype=np.float32)
-        dg.create_dataset("0/afferent_segment_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_segment_offset", data=[0] * count, dtype=int)
-
-        dg.create_dataset("0/n_rrp_vesicles", data=[4] * count, dtype=int)
-        dg.create_dataset("0/syn_type_id", data=[31] * count, dtype=int)
-
-        ds = h5.create_dataset(f"/edges/{name}/source_node_id", data=np.array([0], dtype=int))
-        ds.attrs["node_population"] = "RingA"
-        ds = h5.create_dataset(f"/edges/{name}/target_node_id", data=np.array([0], dtype=int))
-        ds.attrs["node_population"] = "RingB"
+        ds = dg.create_dataset("source_node_id", data=np.array(src_ids, dtype=int))
+        ds.attrs["node_population"] = edges.src
+        ds = dg.create_dataset("target_node_id", data=np.array(tgt_ids, dtype=int))
+        ds.attrs["node_population"] = edges.tgt
 
     libsonata.EdgePopulation.write_indices(
-        path,
+        filename,
         name,
         source_node_count=count,
         target_node_count=count,
     )
+
+
+
+def make_ringtest_nodes():
+    wanted = {
+        "node_type_id": -1,
+        "model_template": "hoc:B_BallStick",
+        "mtype": "MTYPE",  # neurodamus/io/cell_readers.py:140: SonataError
+
+        # neurodamus/io/cell_readers.py:162: SonataError
+        "x": it.count(0),
+        "y": it.count(1),
+        "z": it.count(2),
+
+        # Note: the morphology isn't used because it's encoded in the hoc file
+        "morphology": "NOT_USED",
+        }
+    make_node(filename="nodes_A.h5", name="RingA", count=3, wanted_attributes=wanted)
+
+    wanted = {
+        "node_type_id": -1,
+        "model_template": "hoc:B_BallStick",
+        "mtype": "MTYPE",  # neurodamus/io/cell_readers.py:140: SonataError
+
+        # neurodamus/io/cell_readers.py:162: SonataError
+        "x": it.count(3),
+        "y": it.count(4),
+        "z": it.count(5),
+
+        # Note: the morphology isn't used because it's encoded in the hoc file
+        "morphology": "NOT_USED",
+        }
+    make_node(filename="nodes_B.h5", name="RingB", count=2, wanted_attributes=wanted)
 
 
 def make_edges_A():
-    name = "RingA__RingA__chemical"
-    count = 3
-    path = "local_edges_A.h5"
-    with h5py.File(path, "w") as h5:
-        dg = h5.create_group(f"/edges/{name}")
-        dg.create_dataset("edge_type_id", data=[-1] * count)
+    edges = Edges("RingA", "RingA", "chemical", [(0, 1), (1, 2), (2, 0)])
+    wanted_attributes = {
+        "edge_type_id": -1,
+        "conductance": it.count(11.),
+        "decay_time": it.count(12.),
+        "delay": it.count(13.),
+        "depression_time": it.count(14.),
+        "facilitation_time": it.count(15.),
+        "u_syn": it.count(16.),
 
-        dg.create_dataset("0/conductance", data=[11., 12., 13.], dtype=np.float32)
-        dg.create_dataset("0/decay_time", data=[12., 13., 14.], dtype=np.float32)
-        dg.create_dataset("0/delay", data=[13., 14., 15.], dtype=np.float32)
-        dg.create_dataset("0/depression_time", data=[14., 15., 16.], dtype=np.float32)
-        dg.create_dataset("0/facilitation_time", data=[15., 16., 17.], dtype=np.float32)
-        dg.create_dataset("0/u_syn", data=[16., 17., 18], dtype=np.float32)
+        "afferent_section_id": 1,
+        "afferent_section_pos": 0.75,
+        "afferent_segment_id": 1,
+        "afferent_segment_offset": 0,
 
-        dg.create_dataset("0/afferent_section_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_section_pos", data=[0.75] * count, dtype=np.float32)
-        dg.create_dataset("0/afferent_segment_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_segment_offset", data=[0] * count, dtype=int)
-
-        dg.create_dataset("0/n_rrp_vesicles", data=[4] * count, dtype=int)
-        dg.create_dataset("0/syn_type_id", data=[60, 104, 77] * count, dtype=int)
-
-        ds = dg.create_dataset("source_node_id", data=np.array([0, 1, 2], dtype=int))
-        ds.attrs["node_population"] = "RingA"
-        ds = dg.create_dataset("target_node_id", data=np.array([1, 2, 0], dtype=int))
-        ds.attrs["node_population"] = "RingA"
-
-    libsonata.EdgePopulation.write_indices(
-        path,
-        name,
-        source_node_count=count,
-        target_node_count=count,
-    )
+        "n_rrp_vesicles": 4,
+        "syn_type_id": [60, 104, 77],
+        }
+    make_edges(filename="local_edges_A.h5", edges=edges, wanted_attributes=wanted_attributes)
 
 
 def make_edges_B():
-    name = "RingB__RingB__chemical"
-    count = 2
-    path = "local_edges_B.h5"
-    with h5py.File(path, "w") as h5:
-        dg = h5.create_group(f"/edges/{name}")
-        dg.create_dataset("edge_type_id", data=[-1] * count)
+    edges = Edges("RingB", "RingB", "chemical", [(0, 1), (1, 0)])
+    wanted_attributes = {
+        "edge_type_id": -1,
+        "conductance": it.count(21.),
+        "decay_time": it.count(22.),
+        "delay": it.count(23.),
+        "depression_time": it.count(24.),
+        "facilitation_time": it.count(25.),
+        "u_syn": it.count(26.),
 
-        dg.create_dataset("0/conductance", data=[21., 22.], dtype=np.float32)
-        dg.create_dataset("0/decay_time", data=[22., 23.], dtype=np.float32)
-        dg.create_dataset("0/delay", data=[23., 24.], dtype=np.float32)
-        dg.create_dataset("0/depression_time", data=[24., 25.], dtype=np.float32)
-        dg.create_dataset("0/facilitation_time", data=[25., 26.], dtype=np.float32)
-        dg.create_dataset("0/u_syn", data=[26., 27.], dtype=np.float32)
+        "afferent_section_id": 1,
+        "afferent_section_pos": 0.75,
+        "afferent_segment_id": 1,
+        "afferent_segment_offset": 0,
 
-        dg.create_dataset("0/afferent_section_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_section_pos", data=[0.75] * count, dtype=np.float32)
-        dg.create_dataset("0/afferent_segment_id", data=[1] * count, dtype=int)
-        dg.create_dataset("0/afferent_segment_offset", data=[0] * count, dtype=int)
-
-        dg.create_dataset("0/n_rrp_vesicles", data=[4] * count, dtype=int)
-        dg.create_dataset("0/syn_type_id", data=[24, 29] * count, dtype=int)
-
-        ds = dg.create_dataset("source_node_id", data=np.array([0, 1], dtype=int))
-        ds.attrs["node_population"] = "RingB"
-        ds = dg.create_dataset("target_node_id", data=np.array([1, 0], dtype=int))
-        ds.attrs["node_population"] = "RingB"
-
-    libsonata.EdgePopulation.write_indices(
-        path,
-        name,
-        source_node_count=count,
-        target_node_count=count,
-    )
+        "n_rrp_vesicles": 4,
+        "syn_type_id": [24, 29],
+        }
+    make_edges(filename="local_edges_B.h5", edges=edges, wanted_attributes=wanted_attributes)
 
 
-make_nodes()
+def make_edges_AB():
+    edges = Edges("RingA", "RingB", "chemical", [(0, 0)])
+    wanted_attributes = {
+        "edge_type_id": -1,
+        "conductance": 1.,
+        "decay_time": 2.,
+        "delay": 3.,
+        "depression_time": 4.,
+        "facilitation_time": 5.,
+        "u_syn": 6.,
+
+        "afferent_section_id": 1,
+        "afferent_section_pos": 0.75,
+        "afferent_segment_id": 1,
+        "afferent_segment_offset": 0,
+        "n_rrp_vesicles": 4,
+        "syn_type_id": 31,
+        }
+    make_edges(filename="edges_AB.h5", edges=edges, wanted_attributes=wanted_attributes)
+
+
+make_ringtest_nodes()
 make_edges_AB()
 make_edges_A()
 make_edges_B()
