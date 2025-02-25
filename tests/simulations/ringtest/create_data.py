359f31dbbf7ea2ddb434364d2ddd9d7e3e888736
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-02-25 14:27:18 +0100

    simplify

--- tests/simulations/ringtest/create_data.py
+++ tests/simulations/ringtest/create_data.py
@@ -4,12 +4,20 @@
 # ///
 # the above allows one to run `uv run create_data.py` without a virtualenv
 import itertools as it
-from dataclasses import dataclass
 from collections.abc import Iterable, Sequence
+from dataclasses import dataclass
 
 import h5py
 import libsonata
 import numpy as np
+
+
+@dataclass
+class Edges:
+    src: str
+    tgt: str
+    type: str
+    connections: list[(int, int)]
 
 
 @dataclass
@@ -27,7 +35,7 @@ NODE_TYPES = [
     SonataAttribute("y", type=np.float32, prefix=True),
     SonataAttribute("z", type=np.float32, prefix=True),
     SonataAttribute("morphology", type=h5py.string_dtype(), prefix=True),
-    ]
+]
 NODE_TYPES = {attr.name: attr for attr in NODE_TYPES}
 
 EDGE_TYPES = [
@@ -38,24 +46,14 @@ EDGE_TYPES = [
     SonataAttribute("depression_time", type=np.float32, prefix=True),
     SonataAttribute("facilitation_time", type=np.float32, prefix=True),
     SonataAttribute("u_syn", type=np.float32, prefix=True),
-
     SonataAttribute("afferent_section_id", type=int, prefix=True),
     SonataAttribute("afferent_section_pos", type=np.float32, prefix=True),
     SonataAttribute("afferent_segment_id", type=int, prefix=True),
     SonataAttribute("afferent_segment_offset", type=int, prefix=True),
-
     SonataAttribute("n_rrp_vesicles", type=int, prefix=True),
     SonataAttribute("syn_type_id", type=int, prefix=True),
-    ]
+]
 EDGE_TYPES = {attr.name: attr for attr in EDGE_TYPES}
-
-
-@dataclass
-class Edges:
-    src: str
-    tgt: str
-    type: str
-    connections: list[(int, int)]
 
 
 def _expand_values(attr, value, count):
@@ -109,105 +107,89 @@ def make_edges(filename, edges, wanted_attributes):
     )
 
 
-
 def make_ringtest_nodes():
     wanted = {
         "node_type_id": -1,
         "model_template": "hoc:B_BallStick",
         "mtype": "MTYPE",  # neurodamus/io/cell_readers.py:140: SonataError
-
         # neurodamus/io/cell_readers.py:162: SonataError
         "x": it.count(0),
         "y": it.count(1),
         "z": it.count(2),
-
         # Note: the morphology isn't used because it's encoded in the hoc file
         "morphology": "NOT_USED",
-        }
+    }
     make_node(filename="nodes_A.h5", name="RingA", count=3, wanted_attributes=wanted)
 
     wanted = {
         "node_type_id": -1,
         "model_template": "hoc:B_BallStick",
         "mtype": "MTYPE",  # neurodamus/io/cell_readers.py:140: SonataError
-
         # neurodamus/io/cell_readers.py:162: SonataError
         "x": it.count(3),
         "y": it.count(4),
         "z": it.count(5),
-
         # Note: the morphology isn't used because it's encoded in the hoc file
         "morphology": "NOT_USED",
-        }
+    }
     make_node(filename="nodes_B.h5", name="RingB", count=2, wanted_attributes=wanted)
 
 
-def make_edges_A():
+def make_ringtest_edges():
     edges = Edges("RingA", "RingA", "chemical", [(0, 1), (1, 2), (2, 0)])
     wanted_attributes = {
         "edge_type_id": -1,
-        "conductance": it.count(11.),
-        "decay_time": it.count(12.),
-        "delay": it.count(13.),
-        "depression_time": it.count(14.),
-        "facilitation_time": it.count(15.),
-        "u_syn": it.count(16.),
-
+        "conductance": it.count(11.0),
+        "decay_time": it.count(12.0),
+        "delay": it.count(13.0),
+        "depression_time": it.count(14.0),
+        "facilitation_time": it.count(15.0),
+        "u_syn": it.count(16.0),
         "afferent_section_id": 1,
         "afferent_section_pos": 0.75,
         "afferent_segment_id": 1,
         "afferent_segment_offset": 0,
-
         "n_rrp_vesicles": 4,
         "syn_type_id": [60, 104, 77],
-        }
+    }
     make_edges(filename="local_edges_A.h5", edges=edges, wanted_attributes=wanted_attributes)
 
-
-def make_edges_B():
     edges = Edges("RingB", "RingB", "chemical", [(0, 1), (1, 0)])
     wanted_attributes = {
         "edge_type_id": -1,
-        "conductance": it.count(21.),
-        "decay_time": it.count(22.),
-        "delay": it.count(23.),
-        "depression_time": it.count(24.),
-        "facilitation_time": it.count(25.),
-        "u_syn": it.count(26.),
-
+        "conductance": it.count(21.0),
+        "decay_time": it.count(22.0),
+        "delay": it.count(23.0),
+        "depression_time": it.count(24.0),
+        "facilitation_time": it.count(25.0),
+        "u_syn": it.count(26.0),
         "afferent_section_id": 1,
         "afferent_section_pos": 0.75,
         "afferent_segment_id": 1,
         "afferent_segment_offset": 0,
-
         "n_rrp_vesicles": 4,
         "syn_type_id": [24, 29],
-        }
+    }
     make_edges(filename="local_edges_B.h5", edges=edges, wanted_attributes=wanted_attributes)
 
-
-def make_edges_AB():
     edges = Edges("RingA", "RingB", "chemical", [(0, 0)])
     wanted_attributes = {
         "edge_type_id": -1,
-        "conductance": 1.,
-        "decay_time": 2.,
-        "delay": 3.,
-        "depression_time": 4.,
-        "facilitation_time": 5.,
-        "u_syn": 6.,
-
+        "conductance": 1.0,
+        "decay_time": 2.0,
+        "delay": 3.0,
+        "depression_time": 4.0,
+        "facilitation_time": 5.0,
+        "u_syn": 6.0,
         "afferent_section_id": 1,
         "afferent_section_pos": 0.75,
         "afferent_segment_id": 1,
         "afferent_segment_offset": 0,
         "n_rrp_vesicles": 4,
         "syn_type_id": 31,
-        }
+    }
     make_edges(filename="edges_AB.h5", edges=edges, wanted_attributes=wanted_attributes)
 
 
 make_ringtest_nodes()
-make_edges_AB()
-make_edges_A()
-make_edges_B()
+make_ringtest_edges()
