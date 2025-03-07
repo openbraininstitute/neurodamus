a82110cbb7d33a06b82299d15a4d0cc5d298a69d
Weina Ji <weina.ji@openbraininstitute.org>
2025-03-07 15:39:26 +0100

    ruff format and lint

--- tests/unit/test_ringcells_reports.py
+++ tests/unit/test_ringcells_reports.py
@@ -1,57 +1,67 @@
-import pytest
 from pathlib import Path
 
+import pytest
+
+from tests.utils import read_ascii_report, record_compartment_report, write_ascii_report
+
 from neurodamus.node import Node
-from tests.utils import record_compartment_report, write_ascii_report, read_ascii_report
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("create_tmp_simulation_config_file", [
-    {
-        "simconfig_fixture": "ringtest_baseconfig",
-        "extra_config": {
-            "reports": {
-                "new_report": {
-                    "type": "compartment",
-                    "cells": "Mosaic",
-                    "variable_name": "wrong",
-                    "sections": "all",
-                    "dt": 0.1,
-                    "start_time": 0.0,
-                    "end_time": 40.0,
+@pytest.mark.parametrize(
+    "create_tmp_simulation_config_file",
+    [
+        {
+            "simconfig_fixture": "ringtest_baseconfig",
+            "extra_config": {
+                "reports": {
+                    "new_report": {
+                        "type": "compartment",
+                        "cells": "Mosaic",
+                        "variable_name": "wrong",
+                        "sections": "all",
+                        "dt": 0.1,
+                        "start_time": 0.0,
+                        "end_time": 40.0,
+                    }
                 }
-            }
+            },
         }
-    }
-], indirect=True)
+    ],
+    indirect=True,
+)
 def test_report_config_error(create_tmp_simulation_config_file):
-    with pytest.raises(Exception):
-        n = Node(create_tmp_simulation_config_file)
-        n.load_targets()
-        n.create_cells()
+    n = Node(create_tmp_simulation_config_file)
+    n.load_targets()
+    n.create_cells()
+    with pytest.raises(Exception, match=r"1 reporting errors detected. Terminating"):
         n.enable_reports()
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("create_tmp_simulation_config_file", [
-    {
-        "simconfig_fixture": "ringtest_baseconfig",
-        "extra_config": {
-            "reports": {
-                "new_report": {
-                    "type": "compartment",
-                    "cells": "Mosaic",
-                    "variable_name": "wrong",
-                    "sections": "all",
-                    "dt": 0.1,
-                    "start_time": 0.0,
-                    "end_time": 40.0,
-                    "enabled": False
+@pytest.mark.parametrize(
+    "create_tmp_simulation_config_file",
+    [
+        {
+            "simconfig_fixture": "ringtest_baseconfig",
+            "extra_config": {
+                "reports": {
+                    "new_report": {
+                        "type": "compartment",
+                        "cells": "Mosaic",
+                        "variable_name": "wrong",
+                        "sections": "all",
+                        "dt": 0.1,
+                        "start_time": 0.0,
+                        "end_time": 40.0,
+                        "enabled": False,
+                    }
                 }
-            }
+            },
         }
-    }
-], indirect=True)
+    ],
+    indirect=True,
+)
 def test_report_disabled(create_tmp_simulation_config_file):
     n = Node(create_tmp_simulation_config_file)
     n.load_targets()
@@ -61,38 +71,42 @@ def test_report_disabled(create_tmp_simulation_config_file):
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("create_tmp_simulation_config_file", [
-    {
-        "simconfig_fixture": "ringtest_baseconfig",
-        "extra_config": {
-            "target_simulator": "NEURON",
-            "reports": {
-                "soma_v": {
-                    "type": "compartment",
-                    "cells": "Mosaic",
-                    "variable_name": "v",
-                    "sections": "soma",
-                    "dt": 10,
-                    "start_time": 0.0,
-                    "end_time": 40.0
+@pytest.mark.parametrize(
+    "create_tmp_simulation_config_file",
+    [
+        {
+            "simconfig_fixture": "ringtest_baseconfig",
+            "extra_config": {
+                "target_simulator": "NEURON",
+                "reports": {
+                    "soma_v": {
+                        "type": "compartment",
+                        "cells": "Mosaic",
+                        "variable_name": "v",
+                        "sections": "soma",
+                        "dt": 10,
+                        "start_time": 0.0,
+                        "end_time": 40.0,
+                    },
+                    "compartment_i": {
+                        "type": "compartment",
+                        "cells": "Mosaic",
+                        "variable_name": "i_membrane",
+                        "sections": "all",
+                        "dt": 10,
+                        "start_time": 0.0,
+                        "end_time": 40.0,
+                    },
                 },
-                "compartment_i": {
-                    "type": "compartment",
-                    "cells": "Mosaic",
-                    "variable_name": "i_membrane",
-                    "sections": "all",
-                    "dt": 10,
-                    "start_time": 0.0,
-                    "end_time": 40.0
-                }
-            }
+            },
         }
-    }
-], indirect=True)
+    ],
+    indirect=True,
+)
 def test_neuorn_compartment_report(create_tmp_simulation_config_file):
     from neurodamus import Neurodamus
-    from neurodamus.core.configuration import SimConfig
     from neurodamus.core import NeurodamusCore as Nd
+    from neurodamus.core.configuration import SimConfig
 
     n = Neurodamus(create_tmp_simulation_config_file)
     assert len(n.reports) == 2
@@ -105,8 +119,8 @@ def test_neuorn_compartment_report(create_tmp_simulation_config_file):
     for rep_name, rep_conf in reports_conf.items():
         rep_type = rep_conf["Type"]
         if rep_type == "compartment":
-            ascii_recorders[rep_name] = (record_compartment_report(rep_conf, n._target_manager))
-    Nd.finitialize()   # reinit for the recordings to be registered
+            ascii_recorders[rep_name] = record_compartment_report(rep_conf, n._target_manager)
+    Nd.finitialize()  # reinit for the recordings to be registered
     n.run()
 
     # Write ASCII reports
@@ -127,25 +141,29 @@ def test_neuorn_compartment_report(create_tmp_simulation_config_file):
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("create_tmp_simulation_config_file", [
-    {
-        "simconfig_fixture": "ringtest_baseconfig",
-        "extra_config": {
-            "target_simulator": "NEURON",
-            "reports": {
-                "summation": {
-                    "type": "summation",
-                    "cells": "Mosaic",
-                    "variable_name": "i_membrane, IClamp",
-                    "unit": "nA",
-                    "dt": 10,
-                    "start_time": 0.0,
-                    "end_time": 40.0
-                }
-            }
+@pytest.mark.parametrize(
+    "create_tmp_simulation_config_file",
+    [
+        {
+            "simconfig_fixture": "ringtest_baseconfig",
+            "extra_config": {
+                "target_simulator": "NEURON",
+                "reports": {
+                    "summation": {
+                        "type": "summation",
+                        "cells": "Mosaic",
+                        "variable_name": "i_membrane, IClamp",
+                        "unit": "nA",
+                        "dt": 10,
+                        "start_time": 0.0,
+                        "end_time": 40.0,
+                    }
+                },
+            },
         }
-    }
-], indirect=True)
+    ],
+    indirect=True,
+)
 def test_enable_summation_report(create_tmp_simulation_config_file):
     from neurodamus import Neurodamus
 
