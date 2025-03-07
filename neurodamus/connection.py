413e8738aaf8927f25fe0eabe02bc1b4c5526224
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-03-07 15:41:35 +0100

    check that delay_vec and delayweight_vec are None together

--- neurodamus/connection.py
+++ neurodamus/connection.py
@@ -490,7 +490,9 @@ class Connection(ConnectionBase):
             self._spont_minis = None
 
         # Delayed vecs: release if not used, sort if over 1 value
-        if self._delay_vec is None and self._delayweight_vec is None:
+        if self._delay_vec is None or self._delayweight_vec is None:
+            # they should be set together
+            assert self._delayweight_vec == self._delay_vec
             return
         total_delays = self._delay_vec.size()
         if total_delays == 0:
