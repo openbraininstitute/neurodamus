87afb1ada216bedd1d6ce48393aa6dae5b9a27d3
Alessandro Cattabiani <cattabiani@users.noreply.github.com>
2025-03-10 13:49:10 +0100

    `spont_minis` simple test (#124)
    ## Context
    
    `spont_minis` uses a stochastic process to generate events. Synapses, if
    ready, release generating a voltage change.
    
    ## Scope
    Proper testing of this requires a stochastic test campaign that is out
    of the scope of this PR.
    
    Here I just want to see that I see a voltage change in `RingB` if there
    is a `spont_minis` attached. Without the `spont_minis` no peaks are
    recorded. Thus, checking the precise position of the peaks for this
    `spont_minis` should be sufficient as reproducing this randomly or from
    other sources is very unlikely.
    
    I am also testing that a pont minis is attached to the synapse and that
    in the weight it really serves its type as spont_minis
    
    - [x] remove accidental debug code that remained around
    
    ## Fix
    
    #114 only partially,
    
    
    ## Additional bug fixes
    
    #123 while working on this we noticed that `sim_init` can be called
    twice (it has protection) but `init` does not. I added the same
    protection to `init` too.
    
    ## Review
    * [ ] PR description is complete
    * [ ] Coding style (imports, function length, New functions, classes or
    files) are good
    * [ ] Unit/Scientific test added
    * [ ] Updated Readme, in-code, developer documentation

--- neurodamus/connection.py
+++ neurodamus/connection.py
@@ -490,6 +490,10 @@ class Connection(ConnectionBase):
             self._spont_minis = None
 
         # Delayed vecs: release if not used, sort if over 1 value
+        if self._delay_vec is None or self._delayweight_vec is None:
+            # they should be set together
+            assert self._delayweight_vec == self._delay_vec
+            return
         total_delays = self._delay_vec.size()
         if total_delays == 0:
             self._delay_vec = None
--- tests/unit/test_connection_overrides.py
+++ tests/unit/test_connection_overrides.py
@@ -52,7 +52,7 @@ def test_synapse_change_simple_parameters(create_tmp_simulation_config_file):
             "depression_time": 10003.1,
             "facilitation_time": 10002.1,
             "delay": 10005.1
-            }}
+        }}
     for src_pop, src_raw_gid, tgt_pop, tgt_raw_gid in connections:
         src_gid, tgt_gid, edges, selection = utils.get_edge_data(
             nd, src_pop, src_raw_gid, tgt_pop, tgt_raw_gid)
@@ -174,3 +174,63 @@ def test_synapse_modoverride(create_tmp_simulation_config_file):
         for nc_id, nc in enumerate(nclist):
             utils.check_netcon(src_gid, nc_id, nc, edges, selection, **kwargs)
             utils.check_synapse(nc.syn(), edges, selection, **kwargs)
+
+
+@pytest.mark.parametrize("create_tmp_simulation_config_file", [
+    {
+        "simconfig_fixture": "ringtest_baseconfig",
+        "extra_config": {
+            "target_simulator": "NEURON",
+            "node_set": "Mosaic",
+            "connection_overrides": [
+                {
+                    "name": "A2B",
+                    "source": "RingA",
+                    "target": "RingB",
+                    "spont_minis": 200
+                }
+            ]
+        }
+    },
+], indirect=True)
+def test_spont_minis_simple(create_tmp_simulation_config_file):
+    """Test spont_minis standard behavior.
+
+    This test verifies that a spont_minis is correctly added to
+    the simulation, preserving essential properties from existing
+    netcons while enforcing expected modifications.
+
+    The test is divided in 2 parts:
+    - check that the spont_minis is added, neuron sees it
+    and the other netcons are unaffected
+    - run a simulation and see spikes at roughly the correct
+    firing rate
+    """
+    from neurodamus import Neurodamus
+    from neurodamus.core import NeurodamusCore as Ndc
+    from neurodamus.connection import NetConType
+
+    nd = Neurodamus(create_tmp_simulation_config_file)
+    # get all the netcons targetting 1001 from neuron directly
+    cell = nd._pc.gid2cell(1001)
+    nclist = Ndc.cvode.netconlist("", cell, "")
+    assert len(nclist) == 3
+    # assert that the old netcons are still there
+    assert nclist[0].srcgid() == 1
+    assert nclist[1].srcgid() == 1002
+    # test the spont_minis netcon
+    assert nclist[2].srcgid() == -1
+    assert nclist[2].weight[4] == int(NetConType.NC_SPONTMINI)
+    # weight is set as the weight of the original netcon
+    assert nclist[2].weight[0] == nclist[0].weight[0]
+    # delay is always set to 0.1. Check connection.SpontMinis.create_on
+    assert nclist[2].delay == pytest.approx(0.1)
+
+    voltage_trace = Ndc.Vector()
+    cell_ringB = nd.circuits.get_node_manager("RingB").get_cell(1001)
+    voltage_trace.record(cell_ringB._cellref.soma[0](0.5)._ref_v)
+    Ndc.finitialize()  # reinit for the recordings to be registered
+    nd.run()
+
+    # with threshold=1.0 it does not get the last peak
+    utils.check_signal_peaks(voltage_trace, [12, 55, 122, 164, 269, 303, 385], threshold=0.5)
