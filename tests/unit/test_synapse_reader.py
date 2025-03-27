ce5d7e86f839a945b7dcdc69a1dcd59c28cbdead
Weina Ji <weina.ji@openbraininstitute.org>
2025-03-27 15:39:30 +0100

    fix unit tests

--- tests/unit/test_synapse_reader.py
+++ tests/unit/test_synapse_reader.py
@@ -1,8 +1,23 @@
 import numpy as np
+import numpy.testing as npt
 from pathlib import Path
 from unittest.mock import Mock
 
 SIM_DIR = Path(__file__).parent.parent.absolute() / "simulations"
+
+
+def test_gapjunction_sonata_reader():
+    from neurodamus.gap_junction import GapJunctionSynapseReader
+    sonata_file = str(SIM_DIR / "mini_thalamus_sonata/gapjunction/edges.h5")
+    sonata_reader = GapJunctionSynapseReader.create(sonata_file)
+    syn_params_sonata = sonata_reader._load_synapse_parameters(1)
+    ref_junction_id_pre = np.array([10257., 43930., 226003., 298841., 324744.,
+                                    1094745., 1167632., 1172523., 1260104.])
+    ref_junction_id_post = np.array([14., 52., 71., 76., 78., 84., 89., 90., 93.])
+    ref_weight = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+    npt.assert_allclose(syn_params_sonata.efferent_junction_id, ref_junction_id_pre)
+    npt.assert_allclose(syn_params_sonata.afferent_junction_id, ref_junction_id_post)
+    npt.assert_allclose(syn_params_sonata.weight, ref_weight)
 
 
 def test_syn_read_counts():
