93f823a700dcbd71d48cab2f6c3e83e9e28858a2
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-04-25 10:39:36 +0200

    WIP

--- mpi_hello_world.py
+++ mpi_hello_world.py
@@ -5,4 +5,9 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+print(f"MPI vendor: {MPI.get_vendor()}")
+
 print(f"Hello from rank {rank} out of {size}")
+
+if size == 1:
+    print("⚠️ Warning: MPI not properly initialized or only 1 process started")
