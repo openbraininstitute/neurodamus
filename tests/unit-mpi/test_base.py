811f4788035275669b1dae1c9e1dfd802a7aa0b8
Weina Ji <weina.ji@openbraininstitute.org>
2025-04-28 16:54:21 +0200

    live debug session

--- tests/unit-mpi/test_base.py
+++ tests/unit-mpi/test_base.py
@@ -114,5 +114,4 @@ def test_neurodamus(create_tmp_simulation_config_file, mpi_ranks):
     local_gids = n.circuits.get_node_manager("RingA").local_nodes.final_gids()
     import numpy.testing as npt
     npt.assert_allclose(local_gids, local_gids_ref[MPI.rank])
-    if not SimConfig.use_coreneuron:
-        n.run()
+    n.run()
