4003bad869a254681ebc268af43b24ea3dfe21c8
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-04-29 11:23:07 +0200

    rearrange stuff

--- tests/simulations/ringtest/create_data.py
+++ tests/simulations/ringtest/create_data.py
@@ -29,6 +29,7 @@ class Edges:
         if isinstance(self.connections, int):
             n = self.connections
             self.connections = [(i, (i + 1) % n) for i in range(n)]
+
 @dataclass
 class SonataAttribute:
     name: str
@@ -114,7 +115,53 @@ def make_edges(filename, edges, wanted_attributes):
     )
 
 
-#### INPUTS
+def make_lfp_weights():
+    filename = "lfp_file.h5"
+    with h5py.File(filename, "w") as h5:
+        def write_pop(population, node_ids, offsets, scaling_factors):
+            dg = h5.create_group(population)
+            dg.create_dataset("node_ids", data=node_ids)
+            dg.create_dataset("offsets", data=offsets)
+
+            dg = h5.create_group("electrodes/" + population)
+            dg.create_dataset("scaling_factors", dtype='f8', data=scaling_factors)
+
+        node_ids = [0, 1, 2]
+        offsets = [0, 5, 10, 15]
+        scaling_factors = [
+            [0.011, 0.012], 
+            [0.021, 0.022], 
+            [0.031, 0.032], 
+            [0.041, 0.042], 
+            [0.051, 0.052], 
+            [0.061, 0.062],
+            [0.071, 0.072], 
+            [0.081, 0.082], 
+            [0.091, 0.092], 
+            [0.101, 0.102], 
+            [0.111, 0.112], 
+            [0.121, 0.122], 
+            [0.131, 0.132], 
+            [0.141, 0.142], 
+            [0.151, 0.152]
+            ]
+        write_pop("RingA", node_ids, offsets, scaling_factors)
+
+        node_ids = [0, 1]
+        offsets = [0, 5, 10]
+        scaling_factors = [
+            [0.014, 0.015, 0.016], 
+            [0.024, 0.025, 0.026], 
+            [0.034, 0.035, 0.036], 
+            [0.044, 0.045, 0.046], 
+            [0.054, 0.055, 0.056], 
+            [0.064, 0.065, 0.066],
+            [0.074, 0.075, 0.076], 
+            [0.084, 0.085, 0.086], 
+            [0.094, 0.095, 0.096], 
+            [0.104, 0.105, 0.106]
+            ]
+        write_pop("RingB", node_ids, offsets, scaling_factors)
 
 def make_ringtest_nodes(ringA_count, ringB_count, ringC_count):
     """
@@ -266,53 +313,6 @@ def make_ringtest_edges(ringA_count, ringB_count, ringC_count):
     }
     make_edges(filename="local_edges_C.h5", edges=edges, wanted_attributes=wanted_attributes)
 
-def make_lfp_weights():
-    filename = "lfp_file.h5"
-    with h5py.File(filename, "w") as h5:
-        def write_pop(population, node_ids, offsets, scaling_factors):
-            dg = h5.create_group(population)
-            dg.create_dataset("node_ids", data=node_ids)
-            dg.create_dataset("offsets", data=offsets)
-
-            dg = h5.create_group("electrodes/" + population)
-            dg.create_dataset("scaling_factors", dtype='f8', data=scaling_factors)
-
-        node_ids = [0, 1, 2]
-        offsets = [0, 5, 10, 15]
-        scaling_factors = [
-            [0.011, 0.012], 
-            [0.021, 0.022], 
-            [0.031, 0.032], 
-            [0.041, 0.042], 
-            [0.051, 0.052], 
-            [0.061, 0.062],
-            [0.071, 0.072], 
-            [0.081, 0.082], 
-            [0.091, 0.092], 
-            [0.101, 0.102], 
-            [0.111, 0.112], 
-            [0.121, 0.122], 
-            [0.131, 0.132], 
-            [0.141, 0.142], 
-            [0.151, 0.152]
-            ]
-        write_pop("RingA", node_ids, offsets, scaling_factors)
-
-        node_ids = [0, 1]
-        offsets = [0, 5, 10]
-        scaling_factors = [
-            [0.014, 0.015, 0.016], 
-            [0.024, 0.025, 0.026], 
-            [0.034, 0.035, 0.036], 
-            [0.044, 0.045, 0.046], 
-            [0.054, 0.055, 0.056], 
-            [0.064, 0.065, 0.066],
-            [0.074, 0.075, 0.076], 
-            [0.084, 0.085, 0.086], 
-            [0.094, 0.095, 0.096], 
-            [0.104, 0.105, 0.106]
-            ]
-        write_pop("RingB", node_ids, offsets, scaling_factors)
 
 if __name__ == "__main__":
     ringA_count, ringB_count, ringC_count = 3, 2, 3
