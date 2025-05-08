f0823ff7e82d04196c8cfb6835380577e9e11bbd
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-05-08 09:46:27 +0200

    clean up tests

--- tests/unit/test_synapse_reader.py
+++ tests/unit/test_synapse_reader.py
@@ -1,13 +1,21 @@
-import numpy as np
-import numpy.testing as npt
 from pathlib import Path
 from unittest.mock import Mock
+
+import numpy as np
+import numpy.testing as npt
+
+from neurodamus.cell_distributor import CellDistributor
+from neurodamus.connection_manager import ConnectionManagerBase
+from neurodamus.core.nodeset import NodeSet
+from neurodamus.gap_junction import GapJunctionSynapseReader
+from neurodamus.io.synapse_reader import SonataReader
+from neurodamus.target_manager import NodesetTarget
+from neurodamus.utils.memory import DryRunStats
 
 SIM_DIR = Path(__file__).parent.parent.absolute() / "simulations"
 
 
 def test_gapjunction_sonata_reader():
-    from neurodamus.gap_junction import GapJunctionSynapseReader
     sonata_file = SIM_DIR / "mini_thalamus_sonata/gapjunction/edges.h5"
     sonata_reader = GapJunctionSynapseReader(sonata_file)
     syn_params_sonata = sonata_reader._load_synapse_parameters(1)
@@ -21,7 +29,6 @@ def test_gapjunction_sonata_reader():
 
 
 def test_syn_read_counts():
-    from neurodamus.io.synapse_reader import SonataReader
     sonata_file = SIM_DIR / "usecase3/local_edges_A.h5"
     reader = SonataReader(sonata_file, "NodeA__NodeA__chemical")
 
@@ -50,13 +57,6 @@ def test_syn_read_counts():
 def test_conn_manager_syn_stats():
     """Test _get_conn_stats in isolation using a mocked instance of SynapseRuleManager
     """
-    from neurodamus.cell_distributor import CellDistributor
-    from neurodamus.connection_manager import ConnectionManagerBase
-    from neurodamus.core.nodeset import NodeSet
-    from neurodamus.io.synapse_reader import SonataReader
-    from neurodamus.target_manager import NodesetTarget
-    from neurodamus.utils.memory import DryRunStats
-
     sonata_file = SIM_DIR / "usecase3/local_edges_A.h5"
     cell_manager = Mock(CellDistributor)
     cell_manager.population_name = "pop-A"
