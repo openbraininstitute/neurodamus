c0826e447d458cd8ae0a7d520016f86dd10cd79a
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-06-17 11:10:49 +0200

    bring back local_to_global_coord_mapping(

--- neurodamus/metype.py
+++ neurodamus/metype.py
@@ -7,7 +7,7 @@ from os import path as ospath
 import numpy as np
 
 from .core import NeuronWrapper as Nd
-from .core.configuration import SimConfig
+from .core.configuration import ConfigurationError, SimConfig
 
 
 class BaseCell:
@@ -174,6 +174,16 @@ class Cell_V6(METype):  # noqa: N801
         self.local_to_global_matrix = meinfos_v6.local_to_global_matrix
         self.extra_attrs = meinfos_v6.extra_attrs
 
+    def local_to_global_coord_mapping(self, points):
+        if self.local_to_global_matrix is False:
+            raise ConfigurationError(
+                "To use local_to_global_coord_mapping please "
+                "run neurodamus with `enable_coord_mapping=True`"
+            )
+        if self.local_to_global_matrix is None:
+            raise Exception("Nodes don't provide all 3d position/rotation info")
+        return vector_rotate_translate(points, self.local_to_global_matrix)
+
     def delete_axon(self):
         self._cellref.replace_axon()
 
@@ -297,6 +307,24 @@ class METypeItem:
         m[:, 3] = position
         m[:, 3] *= scale
         return m
+
+    def local_to_global_coord_mapping(self, points):
+        return vector_rotate_translate(points, self.local_to_global_matrix)
+
+
+def vector_rotate_translate(points, transform_matrix):
+    """Rotate/translate a vector of 3D points according to a transformation matrix.
+
+    Note: Rotation is done directly using the Einstein Sum method, similarly to scipy,
+        avoiding intermediate states.
+    """
+    if points.shape[0] == 0:
+        return np.array([])
+    if len(points.shape) != 2 or points.shape[1] != 3:
+        raise ValueError("Matrix of input coordinates needs 3 columns.")
+    rot_matrix = transform_matrix[None, :, :3]
+    translation = transform_matrix[:, 3]
+    return np.einsum("ijk,ik->ij", rot_matrix, points) + translation
 
 
 class METypeManager(dict):  # noqa: FURB189
