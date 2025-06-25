d7027df49a2d640a122873f7fe48d7c88980109b
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-06-25 10:09:59 +0200

    test_reports.py: use conftest fixtures
    Fix: #314

--- tests/integration-e2e/test_reports.py
+++ tests/integration-e2e/test_reports.py
@@ -14,70 +14,50 @@ def _read_sonata_report(report_file):
     data = report[pop_name].get()
     return node_ids, data
 
-
-def _create_reports_config(original_config_path: Path, tmp_path: Path) -> tuple[Path, Path]:
-    """
-    Create a modified configuration file in a temporary directory.
-    """
-    # Read the original config file
-    with open(original_config_path, 'r') as f:
-        config = json.load(f)
-
-    # Update the network path in the config
-    config["network"] = str(SIM_DIR / "sub_mini5" / "circuit_config.json")
-
-    # Modify the necessary fields
-    config["reports"] = config.get("reports", {})
-    config["reports"]["summation_report"] = {
-        "type": "summation",
-        "cells": "Mosaic",
-        "variable_name": "i_membrane,IClamp",
-        "sections": "all",
-        "dt": 0.1,
-        "start_time": 0.0,
-        "end_time": 40.0
+@pytest.mark.parametrize("create_tmp_simulation_config_file", [
+    {
+        "simconfig_fixture": "v5_sonata_config",
+        "extra_config": {
+            "node_set": "Mosaic",
+            "reports": {
+                "summation_report": {
+                    "type": "summation",
+                    "cells": "Mosaic",
+                    "variable_name": "i_membrane,IClamp",
+                    "sections": "all",
+                    "dt": 0.1,
+                    "start_time": 0.0,
+                    "end_time": 40.0
+                },
+                "synapse_report" : {
+                    "type": "synapse",
+                    "cells": "Mosaic",
+                    "variable_name": "ProbAMPANMDA_EMS.g",
+                    "sections": "all",
+                    "dt": 0.1,
+                    "start_time": 0.0,
+                    "end_time": 40.0
+                },
+                "summation_ProbGABAAB": {
+                    "type": "summation",
+                    "cells": "Mosaic",
+                    "variable_name": "ProbGABAAB_EMS.i",
+                    "sections": "all",
+                    "dt": 0.1,
+                    "start_time": 0.0,
+                    "end_time": 40.0
+                }
+            }
+        }
     }
-    config["reports"]["synapse_report"] = {
-        "type": "synapse",
-        "cells": "Mosaic",
-        "variable_name": "ProbAMPANMDA_EMS.g",
-        "sections": "all",
-        "dt": 0.1,
-        "start_time": 0.0,
-        "end_time": 40.0
-    }
-    # Added to verify no exception is raised when point process is not present in a section
-    config["reports"]["summation_ProbGABAAB"] = {
-        "type": "summation",
-        "cells": "Mosaic",
-        "variable_name": "ProbGABAAB_EMS.i",
-        "sections": "all",
-        "dt": 0.1,
-        "start_time": 0.0,
-        "end_time": 40.0
-    }
-
-    # Write the modified configuration to a temporary file in tmp_path
-    temp_config_path = tmp_path / "reports_config.json"
-    with open(temp_config_path, "w") as f:
-        json.dump(config, f, indent=4)
-
-    output_dir = Path(config["output"]["output_dir"])
-    if not output_dir.is_absolute():
-        output_dir = tmp_path / output_dir
-
-    return str(temp_config_path), str(output_dir)
-
-
+], indirect=True)
 @pytest.mark.slow
-def test_v5_sonata_reports(tmp_path):
+def test_v5_sonata_reports(create_tmp_simulation_config_file):
     import numpy.testing as npt
     from neurodamus import Neurodamus
+    from neurodamus.core.configuration import SimConfig
 
-    config_file = SIM_DIR / "simulation_config_mini.json"
-    temp_config_path, output_dir = _create_reports_config(config_file, tmp_path)
-
-    nd = Neurodamus(temp_config_path)
+    nd = Neurodamus(create_tmp_simulation_config_file)
     nd.run()
 
     report_refs = {
@@ -90,7 +70,7 @@ def test_v5_sonata_reports(tmp_path):
 
     # Go through each report and compare the results
     for report_name, refs in report_refs.items():
-        result_ids, result_data = _read_sonata_report(Path(output_dir) / report_name)
+        result_ids, result_data = _read_sonata_report(Path(SimConfig.output_root) / report_name)
         assert result_ids == node_id_refs
         for row, col, ref in refs:
             npt.assert_allclose(result_data.data[row][col], ref)
