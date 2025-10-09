b9a49a95e861c13c7a3acf5e5e70cd8704d2b2ee
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-10-09 16:10:55 +0200

    done? all test passing (maybe)

--- tests/unit-ngv-mpi/test_distribution.py
+++ tests/unit-ngv-mpi/test_distribution.py
@@ -56,10 +56,10 @@ def test_distribution(create_tmp_simulation_config_file, mpi_ranks):
     # Check AstrocytesA spikes
     if rank == 0:
         spike_gid_ref = np.array([0, 2])
-        timestamps_ref = np.array([5.45, 7.65])
+        timestamps_ref = np.array([5.5, 7.675])
     elif rank == 1:
         spike_gid_ref = np.array([1, 3])
-        timestamps_ref = np.array([6.7, 8.775])
+        timestamps_ref = np.array([6.725, 8.775])
     astrocyteA_spikes = n._spike_vecs[1]
     timestamps = np.array(astrocyteA_spikes[0])
     spike_gids = np.array(astrocyteA_spikes[1])
