87eb8e0644cf7697bfa578865096ec22c7721364
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-10-09 11:15:13 +0200

    fix multicycle_run

--- tests/integration-e2e/test_multicycle_runs.py
+++ tests/integration-e2e/test_multicycle_runs.py
@@ -28,9 +28,7 @@ def test_v5_sonata_multisteps(capsys, create_tmp_simulation_config_file):
     spike_gids = np.array([
         4, 2, 0
     ])  # 0-based
-    timestamps = np.array([
-        33.425, 37.35, 39.725
-    ])
+    timestamps = np.array([32.275, 37.2  , 39.15 ])
     spike_file = Path(nd._run_conf.get("OutputRoot")) / nd._run_conf.get("SpikesFile")
     obtained_timestamps, obtained_spike_gids = read_sonata_spike_file(spike_file)
     npt.assert_allclose(spike_gids, obtained_spike_gids)
@@ -59,7 +57,7 @@ def test_usecase3_sonata_multisteps(create_tmp_simulation_config_file):
     ])  # 0-based
     timestamps = np.array([
         0.2, 0.3, 0.3, 2.5, 3.4, 4.2, 5.5, 7., 7.4, 8.6, 13.8, 19.6, 25.7, 32., 36.4, 38.5,
-        40.8, 42.6, 45.2, 48.3, 49.9
+        40.9, 42.6, 45.2, 48.3, 49.9
     ])
     spike_file = Path(nd._run_conf.get("OutputRoot")) / nd._run_conf.get("SpikesFile")
     obtained_timestamps, obtained_spike_gids = read_sonata_spike_file(spike_file)
