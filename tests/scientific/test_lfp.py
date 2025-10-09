ae8e71cfaadf6eaea3f71c75bf32d114337b7199
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-10-09 14:27:38 +0200

    now fixed

--- tests/scientific/test_lfp.py
+++ tests/scientific/test_lfp.py
@@ -56,80 +56,80 @@ def test_weights_file(tmp_path):
     return test_file, str(tmp_path / "test_file.h5")
 
 
-# def test_load_lfp_config(test_weights_file):
-#     """
-#     Test that the 'load_lfp_config' function opens and loads correctly
-#     the LFP weights file and checks its format
-#     """
-#     from neurodamus.cell_distributor import LFPManager
-#     from neurodamus.core.configuration import ConfigurationError
+def test_load_lfp_config(test_weights_file):
+    """
+    Test that the 'load_lfp_config' function opens and loads correctly
+    the LFP weights file and checks its format
+    """
+    from neurodamus.cell_distributor import LFPManager
+    from neurodamus.core.configuration import ConfigurationError
 
-#     # Load the electrodes file
-#     _, lfp_weights_file = test_weights_file
+    # Load the electrodes file
+    _, lfp_weights_file = test_weights_file
 
-#     # Create an instance of the class
-#     lfp = LFPManager()
-#     pop_list = ["wrong_pop", "default"]
+    # Create an instance of the class
+    lfp = LFPManager()
+    pop_list = ["wrong_pop", "default"]
 
-#     # Test loading LFP configuration from file
-#     lfp.load_lfp_config(lfp_weights_file, pop_list)
-#     assert lfp._lfp_file
-#     assert isinstance(lfp._lfp_file, h5py.File)
-#     assert "/electrodes/default" in lfp._lfp_file
-#     assert "/default/node_ids" in lfp._lfp_file
+    # Test loading LFP configuration from file
+    lfp.load_lfp_config(lfp_weights_file, pop_list)
+    assert lfp._lfp_file
+    assert isinstance(lfp._lfp_file, h5py.File)
+    assert "/electrodes/default" in lfp._lfp_file
+    assert "/default/node_ids" in lfp._lfp_file
 
-#     del lfp._lfp_file["default"]["node_ids"]
-#     with pytest.raises(ConfigurationError):
-#         lfp.load_lfp_config(lfp_weights_file, pop_list)
+    del lfp._lfp_file["default"]["node_ids"]
+    with pytest.raises(ConfigurationError):
+        lfp.load_lfp_config(lfp_weights_file, pop_list)
 
-#     # Test loading LFP configuration from invalid file
-#     lfp_weights_invalid_file = "./invalid_file.h5"
-#     with pytest.raises(ConfigurationError):
-#         lfp.load_lfp_config(lfp_weights_invalid_file, pop_list)
-
-
-# def test_read_lfp_factors(test_weights_file):
-#     """
-#     Test that the 'read_lfp_factors' function correctly extracts the LFP factors
-#     for the specified gid and section ids from the weights file
-#     """
-#     from neurodamus.cell_distributor import LFPManager
-#     # Create an instance of the class
-#     lfp = LFPManager()
-#     lfp._lfp_file, _ = test_weights_file
-#     # Test the function with valid input (node_id is 0 based, so expected 42 in the file)
-#     gid = 43
-#     result = lfp.read_lfp_factors(gid, ("default", 0)).to_python()
-#     expected_result = [0.1, 0.2, 0.3, 0.4]
-#     assert result == expected_result, f'Expected {expected_result}, but got {result}'
-
-#     # Test the function with invalid input (non-existent gid)
-#     gid = 420
-#     result = lfp.read_lfp_factors(gid, ("default", 0)).to_python()
-#     expected_result = []
-#     assert result == expected_result, f'Expected {expected_result}, but got {result}'
+    # Test loading LFP configuration from invalid file
+    lfp_weights_invalid_file = "./invalid_file.h5"
+    with pytest.raises(ConfigurationError):
+        lfp.load_lfp_config(lfp_weights_invalid_file, pop_list)
 
 
-# def test_number_electrodes(test_weights_file):
-#     """
-#     Test that the 'get_number_electrodes' function correctly extracts the number of
-#     electrodes in the weights file for a certain gid
-#     """
-#     from neurodamus.cell_distributor import LFPManager
-#     # Create an instance of the class
-#     lfp = LFPManager()
-#     lfp._lfp_file, _ = test_weights_file
-#     # Test the function with valid input
-#     gid = 1
-#     result = lfp.get_number_electrodes(gid, ("default", 0))
-#     expected_result = 2
-#     assert result == expected_result, f'Expected {expected_result}, but got {result}'
+def test_read_lfp_factors(test_weights_file):
+    """
+    Test that the 'read_lfp_factors' function correctly extracts the LFP factors
+    for the specified gid and section ids from the weights file
+    """
+    from neurodamus.cell_distributor import LFPManager
+    # Create an instance of the class
+    lfp = LFPManager()
+    lfp._lfp_file, _ = test_weights_file
+    # Test the function with valid input (node_id is 0 based, so expected 42 in the file)
+    gid = 42
+    result = lfp.read_lfp_factors(gid, ("default", 0)).to_python()
+    expected_result = [0.1, 0.2, 0.3, 0.4]
+    assert result == expected_result, f'Expected {expected_result}, but got {result}'
 
-#     # Test the function with invalid input (non-existent gid)
-#     gid = 420
-#     result = lfp.get_number_electrodes(gid, ("default", 0))
-#     expected_result = 0
-#     assert result == expected_result, f'Expected {expected_result}, but got {result}'
+    # Test the function with invalid input (non-existent gid)
+    gid = 419
+    result = lfp.read_lfp_factors(gid, ("default", 0)).to_python()
+    expected_result = []
+    assert result == expected_result, f'Expected {expected_result}, but got {result}'
+
+
+def test_number_electrodes(test_weights_file):
+    """
+    Test that the 'get_number_electrodes' function correctly extracts the number of
+    electrodes in the weights file for a certain gid
+    """
+    from neurodamus.cell_distributor import LFPManager
+    # Create an instance of the class
+    lfp = LFPManager()
+    lfp._lfp_file, _ = test_weights_file
+    # Test the function with valid input
+    gid = 0
+    result = lfp.get_number_electrodes(gid, ("default", 0))
+    expected_result = 2
+    assert result == expected_result, f'Expected {expected_result}, but got {result}'
+
+    # Test the function with invalid input (non-existent gid)
+    gid = 419
+    result = lfp.get_number_electrodes(gid, ("default", 0))
+    expected_result = 0
+    assert result == expected_result, f'Expected {expected_result}, but got {result}'
 
 
 def _read_sonata_lfp_file(lfp_file):
@@ -176,82 +176,82 @@ def test_v5_sonata_lfp(test_weights_file, create_simulation_config_file_factory,
     nd = Neurodamus(config_file)
     nd.run()
 
-    # # compare results with refs
-    # t3_data = np.array([0.00027065672, -0.00086610153, 0.0014563566, -0.0046603414])
-    # t7_data = np.array([0.00029265403, -0.0009364929, 0.001548515, -0.004955248])
-    # node_ids = np.array([0, 4])
-    # result_ids, result_data = _read_sonata_lfp_file(
-    #     Path(CoreConfig.output_root) / "lfp.h5")["default"]
+    # compare results with refs
+    t3_data = np.array([ 0.00028025, -0.0008968 ,  0.0014093 , -0.00450975])
+    t7_data = np.array([ 0.00030916, -0.0009893 ,  0.00153002, -0.00489607])
+    node_ids = np.array([0, 4])
+    result_ids, result_data = _read_sonata_lfp_file(
+        Path(CoreConfig.output_root) / "lfp.h5")["default"]
 
-    # npt.assert_allclose(result_data.data[3], t3_data)
-    # npt.assert_allclose(result_data.data[7], t7_data)
-    # npt.assert_allclose(result_ids, node_ids)
+    npt.assert_allclose(result_data.data[3], t3_data, atol=1e-8)
+    npt.assert_allclose(result_data.data[7], t7_data, atol=1e-8)
+    npt.assert_allclose(result_ids, node_ids)
 
 
-# @pytest.mark.parametrize("create_tmp_simulation_config_file", [
-#     {
-#         "simconfig_fixture": "ringtest_baseconfig",
-#         "extra_config": {
-#             "target_simulator": "CORENEURON",
-#             "run": {
-#                 "electrodes_file": str(RINGTEST_DIR / "lfp_file.h5")
-#             },
-#             "reports": {
-#                 "lfp_report": {
-#                     "type": "lfp",
-#                     "cells": "Mosaic",
-#                     "variable_name": "v",
-#                     "dt": 0.1,
-#                     "start_time": 0.0,
-#                     "end_time": 2.0
-#                 }
-#             },
-#             "inputs": {
-#                 "stimulus_pulse": {
-#                     "module": "pulse",
-#                     "input_type": "current_clamp",
-#                     "delay": 1,
-#                     "duration": 50,
-#                     "node_set": "RingA",
-#                     "represents_physical_electrode": True,
-#                     "amp_start": 10,
-#                     "width": 1,
-#                     "frequency": 50
-#                 }
-#             }
-#         }
-#     },
-# ], indirect=True)
-# def test_ringcircuit_lfp(create_tmp_simulation_config_file):
-#     import numpy.testing as npt
-#     from neurodamus import Neurodamus
-#     from neurodamus.core.coreneuron_configuration import CoreConfig
+@pytest.mark.parametrize("create_tmp_simulation_config_file", [
+    {
+        "simconfig_fixture": "ringtest_baseconfig",
+        "extra_config": {
+            "target_simulator": "CORENEURON",
+            "run": {
+                "electrodes_file": str(RINGTEST_DIR / "lfp_file.h5")
+            },
+            "reports": {
+                "lfp_report": {
+                    "type": "lfp",
+                    "cells": "Mosaic",
+                    "variable_name": "v",
+                    "dt": 0.1,
+                    "start_time": 0.0,
+                    "end_time": 2.0
+                }
+            },
+            "inputs": {
+                "stimulus_pulse": {
+                    "module": "pulse",
+                    "input_type": "current_clamp",
+                    "delay": 1,
+                    "duration": 50,
+                    "node_set": "RingA",
+                    "represents_physical_electrode": True,
+                    "amp_start": 10,
+                    "width": 1,
+                    "frequency": 50
+                }
+            }
+        }
+    },
+], indirect=True)
+def test_ringcircuit_lfp(create_tmp_simulation_config_file):
+    import numpy.testing as npt
+    from neurodamus import Neurodamus
+    from neurodamus.core.coreneuron_configuration import CoreConfig
 
-#     nd = Neurodamus(create_tmp_simulation_config_file)
-#     nd.run()
+    nd = Neurodamus(create_tmp_simulation_config_file)
+    nd.run()
 
-#     # compare results with refs
-#     lfp_data = _read_sonata_lfp_file(Path(CoreConfig.output_root) / "lfp_report.h5")
-#     result_ids, result_data = lfp_data["RingA"]
+    # compare results with refs
+    lfp_data = _read_sonata_lfp_file(Path(CoreConfig.output_root) / "lfp_report.h5")
+    result_ids, result_data = lfp_data["RingA"]
 
     
 
-#     node_ids = np.array([0, 1, 2])
-#     t11_data = np.array([0.11541528, 0.12541528, 0.6154153, 0.62541527, 1.1154152, 1.1254153])
-#     t19_data = np.array([0.11362588, 0.12362587, 0.6136259, 0.6236259, 1.1136259, 1.1236259])
+    node_ids = np.array([0, 1, 2])
+    t11_data = np.array([0.11541528, 0.12541528, 0.6154153, 0.62541527, 1.1154152, 1.1254153])
+    t19_data = np.array([0.11362588, 0.12362587, 0.6136259, 0.6236259, 1.1136259, 1.1236259])
 
-#     npt.assert_allclose(result_ids, node_ids)
-#     npt.assert_allclose(result_data.data[11], t11_data)
-#     npt.assert_allclose(result_data.data[19], t19_data)
+    npt.assert_allclose(result_ids, node_ids)
+    npt.assert_allclose(result_data.data[11], t11_data)
+    npt.assert_allclose(result_data.data[19], t19_data)
 
-#     result_ids, result_data = lfp_data["RingB"]
+    result_ids, result_data = lfp_data["RingB"]
 
-#     node_ids = np.array([0, 1])
-#     t11_data = np.array(
-#         [6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07])
-#     t19_data = np.array(
-#         [8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07])
+    node_ids = np.array([0, 1])
+    t11_data = np.array(
+        [6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07, 6.4121537e-07])
+    t19_data = np.array(
+        [8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07, 8.2200177e-07])
 
-#     npt.assert_allclose(result_ids, node_ids)
-#     npt.assert_allclose(result_data.data[11], t11_data)
-#     npt.assert_allclose(result_data.data[19], t19_data)
+    npt.assert_allclose(result_ids, node_ids)
+    npt.assert_allclose(result_data.data[11], t11_data)
+    npt.assert_allclose(result_data.data[19], t19_data)
