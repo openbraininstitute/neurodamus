3cd314557489452e2c8de4bae9ece50c824dd30f
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2026-01-09 11:34:49 +0100

    reinstate comments

--- neurodamus/__init__.py
+++ neurodamus/__init__.py
@@ -1,10 +1,13 @@
 """neurodamus
 
 The neurodamus package implements the instantiation of simulations in Neuron
-based on a configuration file, a.k.a. simulation_config.json.
+based on a configuration file, a.k.a. simulation_config.json
+It is deeply based on the HOC implementation, therefore providing python modules like
+`node`, `cell_distributor`, etc; and still depends on several low-level HOC files at runtime.
 """
 
-from ._metadata import __version__
+
 from .node import Neurodamus, Node
+from ._metadata import __version__
 
 __all__ = ["Neurodamus", "Node", "__version__"]
