4edef9601c6124f23b35471d317e50cbcf071ab9
Judit Planas <judit.planas@openbraininstitute.org>
2026-02-13 10:51:16 +0100

    Fix ruff

--- neurodamus/connection_manager.py
+++ neurodamus/connection_manager.py
@@ -405,9 +405,8 @@ class ConnectionManagerBase:
             return
 
         # if we have a single connect block with weight=0, skip synapse creation entirely
-        if (
-            len(matching_conns) == 1 and
-            math.isclose(matching_conns[0].get("Weight"), 0.0, abs_tol=1e-12)
+        if len(matching_conns) == 1 and math.isclose(
+            matching_conns[0].get("Weight"), 0.0, abs_tol=1e-12
         ):
             logging.warning("SKIPPING Connection create since they have invariably weight=0")
             return
