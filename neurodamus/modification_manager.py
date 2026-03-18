136939257c4bd47acc2dfa2bff90ae8ceaeb6967
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2026-03-18 11:13:34 +0100

    lint

--- neurodamus/modification_manager.py
+++ neurodamus/modification_manager.py
@@ -242,6 +242,7 @@ class BaseSectionModification(BaseASTModification):
         allowed = ", ".join(sorted(self.SECTION_TYPES))
         raise ConfigurationError(f"Unknown section type: {name}. Allowed types are: {allowed}")
 
+
 @ModificationManager.register_type
 class SectionList(BaseSectionModification):
     """Perform one or more assignments involving section attributes,
