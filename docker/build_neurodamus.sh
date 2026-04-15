5a8e1250f06d9b8b370351320d0bfcc94e6bd648
Weina Ji <weina.ji@openbraininstitute.org>
2026-04-15 14:52:19 +0200

    switch to openmpi to avoid unexpected memory result in test_dry_run

--- docker/build_neurodamus.sh
+++ docker/build_neurodamus.sh
@@ -3,8 +3,8 @@
 echo "build models from folder " $1
 
 ARCH="x86_64"
-NRNIVMODL_INCLUDE_FLAGS="-I${SONATAREPORT_DIR}/include -I/usr/include/hdf5/mpich -I/usr/lib/${ARCH}-linux-gnu/mpich"
-NRNIVMODL_LOAD_FLAGS="-L${SONATAREPORT_DIR}/lib -lsonatareport -Wl,-rpath,${SONATAREPORT_DIR}/lib -L/usr/lib/${ARCH}-linux-gnu/hdf5/mpich -lhdf5 -Wl,-rpath,/usr/lib/${ARCH}-linux-gnu/hdf5/mpich/ -L/usr/lib/${ARCH}-linux-gnu/ -lmpich -Wl,-rpath,/usr/lib/${ARCH}-linux-gnu/"
+NRNIVMODL_INCLUDE_FLAGS="-I${SONATAREPORT_DIR}/include -I/usr/include/hdf5/openmpi -I/usr/lib/${ARCH}-linux-gnu/openmpi"
+NRNIVMODL_LOAD_FLAGS="-L${SONATAREPORT_DIR}/lib -lsonatareport -Wl,-rpath,${SONATAREPORT_DIR}/lib -L/usr/lib/${ARCH}-linux-gnu/hdf5/openmpi -lhdf5 -Wl,-rpath,/usr/lib/${ARCH}-linux-gnu/hdf5/openmpi/ -L/usr/lib/${ARCH}-linux-gnu/ -lmpi -Wl,-rpath,/usr/lib/${ARCH}-linux-gnu/"
 
 if [[ "$2" == "--only-neuron" ]]; then
     nrnivmodl -incflags ${NRNIVMODL_INCLUDE_FLAGS} -loadflags ${NRNIVMODL_LOAD_FLAGS} "$1"
