e33ac5cec9ed4e178c3b31877e40c3e3fe8975c0
Mike Gevaert <mgevaert@openbraininstitute.org>
2026-05-19 10:44:09 +0200

    asdf

--- ci/scripts/install-sccache.sh
+++ ci/scripts/install-sccache.sh
@@ -4,9 +4,10 @@ install-sccache() {
     local VERSION=v0.15.0
 	local ARCH=$(uname -m)
 
+    set -x
     if [[ -n $SCCACHE_DIR ]]; then
  	 curl -fsSL https://github.com/mozilla/sccache/releases/download/${VERSION}/sccache-${VERSION}-${ARCH}-unknown-linux-musl.tar.gz \
- 		 | tar xz --strip-components=1 -C /usr/bin sccache-${VERSION}-${ARCH}-unknown-linux-musl/sccache
+ 		 | $SUDO tar xz --strip-components=1 -C /usr/bin sccache-${VERSION}-${ARCH}-unknown-linux-musl/sccache
     fi
     sccache --version
 }
